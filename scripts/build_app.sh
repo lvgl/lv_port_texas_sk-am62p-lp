f93a332c58358323e985ae886b67c389f17d56ab
Gabriel Catel Torres <gabriel.catel@edgemtech.ch>
2024-10-02 15:04:47 +0200

    [build]: fix application build system

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Navigate to the /app directory inside the Docker container
-cd /app
-
-# Fix becnhmark demo slow render
-cp scripts/img_benchmark_cogwheel_rgb.c lvgl_port_linux/lvgl/demos/benchmark/assets/
-
 cd /app/lvgl_port_linux
 
-# Set up the build directory and run CMake and Make commands
 cmake -B build-arm64 -S . \
       -DCMAKE_CXX_FLAGS="-O3" \
       -DCMAKE_C_FLAGS="-O3" \
--- scripts/docker_setup.sh
+++ scripts/docker_setup.sh
@@ -7,15 +7,15 @@ ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 cd "$ROOT_DIR"
 
 # Check if the correct argument is passed
-if [[ "$1" == "--build" ]]; then
+if [[ "$1" == "--create-image" ]]; then
     echo "Building the Docker container..."
     docker build --platform linux/arm64/v8 -t lvgl-build-arm64-image . 
 
-elif [[ "$1" == "--run" ]]; then
+elif [[ "$1" == "--build-app" ]]; then
     echo "Running the Docker container..."
     docker run --rm --platform linux/arm64/v8 -v $(pwd):/app lvgl-build-arm64-image
 
 else
-    echo "Usage: $0 --build or --run"
+    echo "Usage: $0 --create-image or --build-app"
     exit 1
 fi
